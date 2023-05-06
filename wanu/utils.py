ae6cbeb1565f3657480aefc37779f1178fcb6c34
nozwock <57829219+nozwock@users.noreply.github.com>
2023-05-06 11:29:51 +0530

    Add `get_content_type` func

--- wanu/utils.py
+++ wanu/utils.py
@@ -1,9 +1,30 @@
 import os
 from pathlib import Path
 from typing import Iterable
+from enum import Enum
+import subprocess
+import re
+from wanu.defines import HACTOOL
 
 
 def get_files_with_ext(from_dir: Path, ext: str) -> Iterable[Path]:
     for file in os.listdir(from_dir):
         if file.endswith(ext):
             yield Path(file)
+
+
+class ContentType(Enum):
+    Program = (0x00,)
+    Meta = (0x01,)
+    Control = (0x02,)
+    Manual = (0x03,)
+    Data = (0x04,)
+    PublicData = (0x05,)
+
+
+def get_content_type(rom: Path) -> str:
+    output = subprocess.check_output([HACTOOL, rom], universal_newlines=True)
+    content_type = re.search(r"Content Type:\s{23}(.*)", output)
+    if content_type:
+        return content_type.group(1).strip()
+    raise Exception("Failed to process ContentType")
