8a23f25eae2fe90403510c3075433130c761fae4
nozwock <57829219+nozwock@users.noreply.github.com>
2023-05-06 10:53:07 +0530

    Add bins in define module

--- wanu/defines.py
+++ wanu/defines.py
@@ -5,3 +5,7 @@ APP_CACHE_DIR = appdirs.user_cache_dir("wanu")
 SWITCH_DIR = Path.home().joinpath(".switch")
 PRODKEYS_PATH = SWITCH_DIR.joinpath("prod.keys")
 TITLEKEY_PATH = SWITCH_DIR.joinpath("title.keys")
+
+HACPACK = Path("../assets/bin/aarch64-linux/hac2l")
+HACTOOL = Path("../assets/bin/aarch64-linux/hac2l")
+HAC2L = Path("../assets/bin/aarch64-linux/hac2l")
