a2fcaaccbc7e870c932f2c9442e451b9ab1f5304
nozwock <57829219+nozwock@users.noreply.github.com>
2023-05-06 20:55:39 +0530

    Fix assets include

--- wanu/defines.py
+++ wanu/defines.py
@@ -1,11 +1,14 @@
 import appdirs
 from pathlib import Path
+import importlib.resources as pkg_resources
+from .assets.bin import aarch64_linux
 
 APP_CACHE_DIR = appdirs.user_cache_dir("wanu")
 SWITCH_DIR = Path.home().joinpath(".switch")
 PRODKEYS_PATH = SWITCH_DIR.joinpath("prod.keys")
 TITLEKEY_PATH = SWITCH_DIR.joinpath("title.keys")
 
-HACPACK = Path("assets/bin/aarch64-linux/hacpack")
-HACTOOL = Path("assets/bin/aarch64-linux/hactool")
-HAC2L = Path("assets/bin/aarch64-linux/hac2l")
+
+HACPACK = pkg_resources.path(aarch64_linux, "hacpack")
+HACTOOL = pkg_resources.path(aarch64_linux, "hactool")
+HAC2L = pkg_resources.path(aarch64_linux, "hac2l")
